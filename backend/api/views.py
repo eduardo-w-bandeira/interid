85007b645bda488288d01b020bb31c414a1027d3
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-08 19:19:41 -0700

    Front and back are communicating

---
+++ backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def test_api(request):
+    data = {
+        "message": "Hello from Django!"
+    }
+    return JsonResponse(data)
