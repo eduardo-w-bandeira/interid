2fe7f31b3f1891428529f289c9d6ab7510cddda3
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-12 18:01:57 -0700

    More route tests in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MultiplyForm from './MultiplyForm';
 import HelloPage from './HelloPage';
+import BlaPage from './BlaPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MultiplyForm />} />
       <Route path="/hello" element={<HelloPage />} />
+      <Route path="/bla" element={<BlaPage />} />
     </Routes>
   );
 };
---
+++ frontend/src/BlaPage.jsx
@@ -0,0 +1,8 @@
+// frontend/src/BlaPage.jsx
+import React from 'react';
+
+const BlaPage = () => {
+    return <div>Bla bla bla!</div>;
+};
+
+export default BlaPage;
