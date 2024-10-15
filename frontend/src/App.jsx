cd06262be861a376e7319ef7ae70dc752fe51616
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-15 13:22:29 -0700

    Small improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MultiplyForm from './MultiplyForm';
+import Home from './pages/Home.jsx';
 import HelloPage from './HelloPage';
 import BlaPage from './BlaPage';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MultiplyForm />} />
+      <Route path="/" element={<Home />} />
       <Route path="/hello" element={<HelloPage />} />
       <Route path="/bla" element={<BlaPage />} />
     </Routes>
