d214dba3e57fc55c1c3a6b0963d13bc6c1234ff7
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-22 10:02:23 -0700

    Add admin

--- backend/master/tests.py
+++ backend/master/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    def test_create_user(self):
+        new_user = User.objects.create(
+            email="bla@example.com",
+            password="securepassword",
+            user_type="individual",
+            gov_id="123456789",
+            gov_id_type="SSN",
+            issuing_authority="Government",
+            country="Canada"
+        )
+        self.assertEqual(new_user.email, "bla@example.com")
