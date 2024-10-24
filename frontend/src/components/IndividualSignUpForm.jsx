b75ee2b5489340801189b99f95e31d1b65692b45
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-24 13:11:34 -0700

    Add CSS for IndividualSignUpForm

--- frontend/src/components/IndividualSignUpForm.jsx
+++ frontend/src/components/IndividualSignUpForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '@/styles/Global.css';
+import '@/styles/IndividualSignUpForm.css';
 
 const IndividualSignUpForm = () => {
     const userType = 'individual';
