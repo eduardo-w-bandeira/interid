c7deb0c26edcb16a41537fab9fda21637d436365
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-23 19:29:42 -0700

    Refactor API URLs to include IndividualView endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('multiply/', multiply, name=multiply.__name__),
     path('users/', UserView.as_view(), name=UserView.__name__),
+    path("individuals/", IndividualView.as_view(), name=IndividualView.__name__),
 ]
 
 # urlpatterns = views.router.get_urlpatterns()
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,49 +3,46 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from databarn import Seed
-import trails
+# from databarn import Seed
+# import trails
 from . models import *
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .ser import UserSer
+from .ser import UserSer, IndividualSer
+
+# router = trails.Router()
 
 
 class UserView(APIView):
 
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSer(users, many=True)
-        return Response(serializer.data)
+        ser = UserSer(users, many=True)
+        return Response(ser.data)
 
     def post(self, request):
-        serializer = UserSer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        ser = UserSer(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=201)
+        return Response(ser.errors, status=400)
 
 
 class IndividualView(APIView):
 
     def get(self, request):
         individuals = Individual.objects.all()
-        serializer = UserSer(individuals, many=True)
-        return Response(serializer.data)
+        ser = IndividualSer(individuals, many=True)
+        return Response(ser.data)
 
     def post(self, request):
-        serializer = UserSer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        ser = IndividualSer(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=201)
+        return Response(ser.errors, status=400)
 
 
-router = trails.Router()
-
-
-@router.autoendpoint()
 @csrf_exempt  # to bypass CSRF for simplicity
 def multiply(request):
     if request.method == 'POST':
@@ -63,97 +60,97 @@ def multiply(request):
         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 
-def create_user(seed: Seed) -> User:
-    user = User.objects.create(
-        email=seed.email,
-        password=seed.password,
-        user_type=seed.user_type,
-        gov_id=seed.gov_id,
-        gov_id_type=seed.gov_id_type,
-        issuing_authority=seed.issuing_authority,
-        country=seed.country
-    )
-    return user
+# def create_user(seed: Seed) -> User:
+#     user = User.objects.create(
+#         email=seed.email,
+#         password=seed.password,
+#         user_type=seed.user_type,
+#         gov_id=seed.gov_id,
+#         gov_id_type=seed.gov_id_type,
+#         issuing_authority=seed.issuing_authority,
+#         country=seed.country
+#     )
+#     return user
 
 
-@router.autoendpoint()
-@csrf_exempt
-@require_POST
-def post_user(request):
-    data: dict = json.loads(request.body)
-    seed = Seed(**data)
-    try:
-        user = create_user(seed)
-        user.save()
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=400)
-    seed.__dna__._create_dynamic_field("id")
-    seed.id = user.id
-    return JsonResponse({'message': f"User saved | {seed}"})
+# @router.autoendpoint()
+# @csrf_exempt
+# @require_POST
+# def post_user(request):
+#     data: dict = json.loads(request.body)
+#     seed = Seed(**data)
+#     try:
+#         user = create_user(seed)
+#         user.save()
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=400)
+#     seed.__dna__._create_dynamic_field("id")
+#     seed.id = user.id
+#     return JsonResponse({'message': f"User saved | {seed}"})
 
 
-@router.autoendpoint()
-@csrf_exempt
-@require_POST
-def post_individual(request):
-    data: dict = json.loads(request.body)
-    seed = Seed(**data)
-    seed.birth_date = trails.str_to_date(seed.birth_date)
-    try:
-        user = create_user(seed)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=400)
-    try:
-        individual = Individual.objects.create(
-            user=user,
-            first_name=seed.first_name,
-            last_name=seed.last_name,
-            birth_date=seed.birth_date)
-        individual.save()
-    except Exception as err:
-        return JsonResponse({'error': str(err)}, status=400)
-    return JsonResponse({'message': f"Individual saved | {seed}"})
+# @router.autoendpoint()
+# @csrf_exempt
+# @require_POST
+# def post_individual(request):
+#     data: dict = json.loads(request.body)
+#     seed = Seed(**data)
+#     seed.birth_date = trails.str_to_date(seed.birth_date)
+#     try:
+#         user = create_user(seed)
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=400)
+#     try:
+#         individual = Individual.objects.create(
+#             user=user,
+#             first_name=seed.first_name,
+#             last_name=seed.last_name,
+#             birth_date=seed.birth_date)
+#         individual.save()
+#     except Exception as err:
+#         return JsonResponse({'error': str(err)}, status=400)
+#     return JsonResponse({'message': f"Individual saved | {seed}"})
 
 
-@router.autoendpoint()
-@csrf_exempt
-@require_POST
-def post_legal_entity(request):
-    data: dict = json.loads(request.body)
-    seed = Seed(**data)
-    seed.reg_date = trails.str_to_date(seed.reg_date)
-    try:
-        user = create_user(seed)
-    except Exception as err:
-        return JsonResponse({'error': str(err)}, status=400)
-    try:
-        legal_entity = LegalEntity.objects.create(
-            user=user,
-            legal_name=seed.legal_name,
-            business_name=seed.business_name,
-            reg_date=seed.reg_date
-        )
-        legal_entity.save()
-    except Exception as err:
-        return JsonResponse({'error': str(err)}, status=400)
-    return JsonResponse({'message': f"Legal entity saved | {seed}"})
+# @router.autoendpoint()
+# @csrf_exempt
+# @require_POST
+# def post_legal_entity(request):
+#     data: dict = json.loads(request.body)
+#     seed = Seed(**data)
+#     seed.reg_date = trails.str_to_date(seed.reg_date)
+#     try:
+#         user = create_user(seed)
+#     except Exception as err:
+#         return JsonResponse({'error': str(err)}, status=400)
+#     try:
+#         legal_entity = LegalEntity.objects.create(
+#             user=user,
+#             legal_name=seed.legal_name,
+#             business_name=seed.business_name,
+#             reg_date=seed.reg_date
+#         )
+#         legal_entity.save()
+#     except Exception as err:
+#         return JsonResponse({'error': str(err)}, status=400)
+#     return JsonResponse({'message': f"Legal entity saved | {seed}"})
 
 
-@router.autoendpoint()
-@csrf_exempt
-@require_POST
-def post_declaration(request):
-    data: dict = json.loads(request.body)
-    seed = Seed(**data)
-    user = User.objects.get(id=seed.user_id)
-    try:
-        declaration = Declaration.objects.create(
-            user=user,
-            title=seed.title,
-            content=seed.content,
-            allow_comments=seed.allow_comments
-        )
-        declaration.save()
-    except Exception as err:
-        return JsonResponse({'error': str(err)}, status=400)
-    return JsonResponse({'message': f"Declaration saved | {seed}"})
+# @router.autoendpoint()
+# @csrf_exempt
+# @require_POST
+# def post_declaration(request):
+#     data: dict = json.loads(request.body)
+#     seed = Seed(**data)
+#     user = User.objects.get(id=seed.user_id)
+#     try:
+#         declaration = Declaration.objects.create(
+#             user=user,
+#             title=seed.title,
+#             content=seed.content,
+#             allow_comments=seed.allow_comments
+#         )
+#         declaration.save()
+#     except Exception as err:
+#         return JsonResponse({'error': str(err)}, status=400)
+#     return JsonResponse({'message': f"Declaration saved | {seed}"})
