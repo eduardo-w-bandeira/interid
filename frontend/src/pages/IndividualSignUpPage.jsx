e501480c5469dd12cf8ddd86a5c09505228bebdc
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-28 16:17:13 -0700

    Transpose IndividualSignUp to Tailwind

--- frontend/src/pages/IndividualSignUpPage.jsx
+++ frontend/src/pages/IndividualSignUpPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Navbar from '@/components/Navbar';
-import IndividualSignUpForm from '../components/IndividualSignUpForm';
+import IndividualSignUpForm from '@/components/IndividualSignUpForm';
 import Footer from '@/components/Footer';
 
 const IndividualSignUpPage = () => {
 
   return (
-    <div className="SignUpPage">
+    <div>
       <Navbar />
       <IndividualSignUpForm />
       <Footer />
