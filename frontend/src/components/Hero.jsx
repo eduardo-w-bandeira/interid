05c695d614fe4af17094d4228964dd049653f6b2
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-28 13:13:22 -0700

    Transpose Hero to Tailwind

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,12 +3,10 @@ import '@/styles/Hero.css';
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <h2>A Global Platform for Formal Civil Actions</h2>
-        <p>Register public declarations, create formal agreements, and access legal services, all with the security of your unique International ID.</p>
-        <button className="btn explore">Explore More</button>
-      </div>
+    <section className="bg-gradient-to-r from-[#06bcec]/90 to-[#ffa724]/90 text-center text-white py-24 px-5">
+        <h2 className='text-5xl'>A Global Platform for Formal Civil Actions</h2>
+        <p className='text-lg mb-8 mt-3'>Register public declarations, create formal agreements, and access legal services, all with the security of your unique International ID.</p>
+        <button className="bg-white text-sky-500 px-8 py-2.5 rounded-md cursor-pointer">Explore More</button>
     </section>
   );
 };
