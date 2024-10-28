9c5597e47fd03dc3d90524ee6c33903f1fd943ad
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-28 13:16:42 -0700

    Adjust Hero colors against built-in tailwind colors

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import '@/styles/Hero.css';
 
 const Hero = () => {
   return (
-    <section className="bg-gradient-to-r from-[#06bcec]/90 to-[#ffa724]/90 text-center text-white py-24 px-5">
+    <section className="bg-gradient-to-r from-sky-500/90 to-amber-400/90 text-center text-white py-24 px-5">
         <h2 className='text-5xl'>A Global Platform for Formal Civil Actions</h2>
         <p className='text-lg mb-8 mt-3'>Register public declarations, create formal agreements, and access legal services, all with the security of your unique International ID.</p>
         <button className="bg-white text-sky-500 px-8 py-2.5 rounded-md cursor-pointer">Explore More</button>
