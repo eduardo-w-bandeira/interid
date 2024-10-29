5b0a614ee24fdf9633598b1236a8e921d1002334
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-28 18:51:08 -0700

    Update footer background color

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-5 text-center">
+    <footer className="bg-gray-900 text-white p-5 text-center">
       <div className="footer-container">
         <p>&copy; 2024 InterId. All rights reserved.</p>
         <ul className="list-none flex justify-center mt-2">
