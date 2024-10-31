a3758d9c55fb2b181b4a33db75445e97bdd9ca69
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-10-30 17:49:27 -0700

    Cosmetic change in import

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import authenticate, login
+from .slizer import LoginSlizer
+from rest_framework import status
 import json
 from django.shortcuts import redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import trails
@@ -17,7 +20,7 @@ def home(request):
     return redirect('api/')
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSlizer
 
@@ -132,99 +135,3 @@ def multiply(request):
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
-
-
-# def create_user(seed: Seed) -> User:
-#     user = User.objects.create(
-#         email=seed.email,
-#         password=seed.password,
-#         user_type=seed.user_type,
-#         gov_id=seed.gov_id,
-#         gov_id_type=seed.gov_id_type,
-#         issuing_authority=seed.issuing_authority,
-#         country=seed.country
-#     )
-#     return user
-
-
-# @router.auto_route()
-# @csrf_exempt
-# @require_POST
-# def post_user(request):
-#     data: dict = json.loads(request.body)
-#     seed = Seed(**data)
-#     try:
-#         user = create_user(seed)
-#         user.save()
-#     except Exception as e:
-#         return JsonResponse({'error': str(e)}, status=400)
-#     seed.__dna__._create_dynamic_field("id")
-#     seed.id = user.id
-#     return JsonResponse({'message': f"User saved | {seed}"})
-
-
-# @router.auto_route()
-# @csrf_exempt
-# @require_POST
-# def post_individual(request):
-#     data: dict = json.loads(request.body)
-#     seed = Seed(**data)
-#     seed.birth_date = trails.str_to_date(seed.birth_date)
-#     try:
-#         user = create_user(seed)
-#     except Exception as e:
-#         return JsonResponse({'error': str(e)}, status=400)
-#     try:
-#         individual = Individual.objects.create(
-#             user=user,
-#             first_name=seed.first_name,
-#             last_name=seed.last_name,
-#             birth_date=seed.birth_date)
-#         individual.save()
-#     except Exception as err:
-#         return JsonResponse({'error': str(err)}, status=400)
-#     return JsonResponse({'message': f"Individual saved | {seed}"})
-
-
-# @router.auto_route()
-# @csrf_exempt
-# @require_POST
-# def post_legal_entity(request):
-#     data: dict = json.loads(request.body)
-#     seed = Seed(**data)
-#     seed.reg_date = trails.str_to_date(seed.reg_date)
-#     try:
-#         user = create_user(seed)
-#     except Exception as err:
-#         return JsonResponse({'error': str(err)}, status=400)
-#     try:
-#         legal_entity = LegalEntity.objects.create(
-#             user=user,
-#             legal_name=seed.legal_name,
-#             business_name=seed.business_name,
-#             reg_date=seed.reg_date
-#         )
-#         legal_entity.save()
-#     except Exception as err:
-#         return JsonResponse({'error': str(err)}, status=400)
-#     return JsonResponse({'message': f"Legal entity saved | {seed}"})
-
-
-# @router.auto_route()
-# @csrf_exempt
-# @require_POST
-# def post_declaration(request):
-#     data: dict = json.loads(request.body)
-#     seed = Seed(**data)
-#     user = User.objects.get(id=seed.user_id)
-#     try:
-#         declaration = Declaration.objects.create(
-#             user=user,
-#             title=seed.title,
-#             content=seed.content,
-#             allow_comments=seed.allow_comments
-#         )
-#         declaration.save()
-#     except Exception as err:
-#         return JsonResponse({'error': str(err)}, status=400)
-#     return JsonResponse({'message': f"Declaration saved | {seed}"})
