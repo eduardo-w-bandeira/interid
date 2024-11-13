364d06e46097f3e63d2ea1286f42315b5f545ab3
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-12 19:16:23 -0800

    Update UserProfile component to display full name based on user type

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -3,6 +3,12 @@ import userImage from '@/assets/generic-user.png';
 
 const UserProfile = ({ userData }) => {
     const relatedUser = userData.related_user;
+    let fullName;
+    if (userData.user_type === "individual") {
+        fullName = `${relatedUser.first_name} ${relatedUser.last_name}`;
+    } else {
+        fullName = `${relatedUser.business_name}`;
+    }
     return (
         <div className="md:w-1/3 p-5 bg-white rounded-lg shadow-lg">
             {relatedUser && (
@@ -12,7 +18,7 @@ const UserProfile = ({ userData }) => {
                         alt="User Profile"
                         className="rounded-full mb-4 w-32 object-cover"
                     />
-                    <h2 className="text-xl font-bold">{relatedUser.first_name} {relatedUser.last_name}</h2>
+                    <h2 className="text-xl font-bold">{fullName}</h2>
                     <p className="text-gray-600">ID: {userData.id}</p>
                 </>
             )}
