a9184bc585b4d55ef46d723e1869e61402aacd4c
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-17 14:17:45 -0800

    Update ActionPanel component to change button label from 'Messages' to 'Register an Agreement'

--- frontend/src/components/ActionPanel.jsx
+++ frontend/src/components/ActionPanel.jsx
@@ -35,7 +35,7 @@ const ActionPanel = ({ userData, postAndShow }) => {
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                         </svg>
-                        <span>Messages</span>
+                        <span>Register an Agreement</span>
                     </a>
                     <a href="#" className="flex items-center text-gray-600 hover:text-gray-800">
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
