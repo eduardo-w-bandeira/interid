130112d6efdcb0d1f27efd85ed2fa1ec660ef2d3
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-16 23:57:37 -0800

    Refactor getCredentials function to simplify logged-in status check and remove unused page name conversion

--- frontend/src/utils/Utils.js
+++ frontend/src/utils/Utils.js
@@ -8,22 +8,15 @@ export const pascalToKebab = (str) => {
     return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 };
 
-export const pageNameToKebab = (name) => {
-    let removePageSuffix = name.replace('Page', '');
-    return pascalToKebab(removePageSuffix);
-};
-
 export const getCredentials = () => {
     const accessToken = localStorage.getItem('access_token');
     const refreshToken = localStorage.getItem('refresh_token');
-    const isLoggedIn = accessToken ? true : false;
     const userDataStr = localStorage.getItem('user_data');
     let userData = null;
     if (userDataStr) {
         userData = JSON.parse(userDataStr);
     };
     return {
-        isLoggedIn: isLoggedIn,
         accessToken: accessToken,
         refreshToken: refreshToken,
         userData: userData
