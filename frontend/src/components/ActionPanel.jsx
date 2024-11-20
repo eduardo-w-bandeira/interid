fdcd9f3173a29ea59e112865a19e443ccbd9b631
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-20 08:53:55 -0800

    Replace anchor tags with buttons

--- frontend/src/components/ActionPanel.jsx
+++ frontend/src/components/ActionPanel.jsx
@@ -21,30 +21,30 @@ const ActionPanel = ({ userData, postAndShow, accessToken }) => {
                         </svg>
                         <span>Home</span>
                     </Link>
-                    <a href="#" className="flex items-center text-gray-600 hover:text-gray-800">
+                    <button className="flex items-center text-gray-600 hover:text-gray-800">
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                         <span>Search</span>
-                    </a>
-                    <a href="#" className="flex items-center text-gray-600 hover:text-gray-800">
+                    </button>
+                    <button className="flex items-center text-gray-600 hover:text-gray-800">
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
                         </svg>
                         <span>Explore</span>
-                    </a>
-                    <a href="#" className="flex items-center text-gray-600 hover:text-gray-800">
+                    </button>
+                    <button className="flex items-center text-gray-600 hover:text-gray-800">
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                         </svg>
                         <span>Notifications</span>
-                    </a>
-                    <a href="#" className="flex items-center text-gray-600 hover:text-gray-800"  onClick={() => setShowAgreementDialog(true)}>
+                    </button>
+                    <button className="flex items-center text-gray-600 hover:text-gray-800"  onClick={() => setShowAgreementDialog(true)}>
                         <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                         </svg>
                         <span>Propose an Agreement</span>
-                    </a>
+                    </button>
                     <button
                         id="DeclarationButton"
                         className="flex items-center text-gray-600 hover:text-gray-800"
