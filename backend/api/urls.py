68e2a065e64c0ee1c9e53b93846bec5d00ab34de
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-21 09:39:00 -0800

    Use wizrouter for cleaner routing

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,20 +1,5 @@
-from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
+# from django.urls import path, include
 from api import views
 
-# rest_router = DefaultRouter()
-# rest_router.register(r'users', views.UserViewSet)
-# rest_router.register(r'individuals', views.IndividualViewSet)
-# rest_router.register(r'legal-entitys', views.LegalEntityViewSet)
-# rest_router.register(r'declarations', views.DeclarationViewSet)
 
-urlpatterns = [
-    # path('', include(rest_router.urls)),
-    # path("individuals/", views.IndividualViews.as_view()),
-    # path("individuals/<int:pk>/", views.IndividualDetail.as_view()),
-    # path("legal-entitys/", views.LegalEntityViews.as_view()),
-    # path("legal-entitys/<int:pk>/", views.LegalEntityDetail.as_view()),
-    # path('login/', views.LoginView.as_view(), name='login')
-]
-
-views.wizrouter.include_urlpatterns(urlpatterns)
+urlpatterns = views.wizrouter.get_urlpatterns()
