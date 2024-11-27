54c243a450f181e975dd12090a85a5d849d17d5b
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-27 11:34:35 -0800

    Add conditional rendering for user

--- frontend/src/components/CommandPanel.jsx
+++ frontend/src/components/CommandPanel.jsx
@@ -34,12 +34,12 @@ const CommandPanel = ({ postAndShowDeclaration, postProposal }) => {
                 <span className="font-bold text-gray-800 text-lg">InterId</span>
             </div>
             <nav className="space-y-5">
-                <Link to={`/${userData.id}`} className="flex items-center text-gray-600 hover:text-gray-800">
+                {userData && (<Link to={`/${userData.id}`} className="flex items-center text-gray-600 hover:text-gray-800">
                     <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
                     <span>My Profile</span>
-                </Link>
+                </Link>)}
                 <button className="flex items-center text-gray-600 hover:text-gray-800">
                     <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
