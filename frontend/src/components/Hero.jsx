87371358b09c924adcdd1171a3a89fe71891044e
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-11-27 05:29:58 -0800

    Update terminology change from "civil actions" to "civil transactions"

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className="bg-gradient-to-r from-sky-500/90 to-amber-400/90 text-center text-white py-24 px-5">
-        <h2 className='text-5xl'>A Global Platform for Formal Civil Actions</h2>
-        <p className='text-lg mb-8 mt-3'>Register public declarations, create formal agreements, and access legal services, all with the security of your unique International ID.</p>
+        <h2 className='text-5xl'>A Global Platform for Civil Transactions</h2>
+        <p className='text-lg mb-8 mt-3'>Register public declarations, formal agreements, all with the security of your unique International ID.</p>
         <button className="bg-white text-sky-500 px-8 py-2.5 rounded-md cursor-pointer">Explore More</button>
     </section>
   );
